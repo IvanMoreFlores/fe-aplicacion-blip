8157bafa67f5d00255b33c0176bd32a76f85510f
Harry Arroyo Preciado <harroyop@gmail.com>
2024-05-13 19:06:52 -0500

    Reparacion CAPACITOR - Harry y Ricardo

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'host.blip.pe',
   appName: 'bliphostfrontend',
   webDir: 'www',
   server: {
