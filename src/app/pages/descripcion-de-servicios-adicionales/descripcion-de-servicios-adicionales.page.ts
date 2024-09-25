416bb4ecac3abbe161672d81dbc901352e83146e
ricardo-gutierrez23 <ricardo.alianzacabrera@gmail.com>
2024-09-24 20:20:50 -0500

    creacion de anuncio,paso2,modal

--- src/app/pages/descripcion-de-servicios-adicionales/descripcion-de-servicios-adicionales.page.ts
+++ src/app/pages/descripcion-de-servicios-adicionales/descripcion-de-servicios-adicionales.page.ts
@@ -6,7 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./descripcion-de-servicios-adicionales.page.scss'],
 })
 export class DescripcionDeServiciosAdicionalesPage implements OnInit {
-
+  isChecked: boolean = false;
+  isChecked2: boolean = false;
+  isChecked3: boolean = false;
+  isChecked4: boolean = false;
+  isChecked5: boolean = false;
+  onChange(event: any) {
+    this.isChecked = event.target.checked;
+    console.log('Checkbox checked:', this.isChecked);
+  }
   constructor() { }
 
   ngOnInit() {
