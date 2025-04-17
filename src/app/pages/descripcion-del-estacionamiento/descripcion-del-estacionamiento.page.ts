47ca80a49b0b7425d80a69788cebfd16209ed057
Juan Gonzales <juan.gonzales@blip.pe>
2025-04-16 23:07:31 -0500

    Fix

--- src/app/pages/descripcion-del-estacionamiento/descripcion-del-estacionamiento.page.ts
+++ src/app/pages/descripcion-del-estacionamiento/descripcion-del-estacionamiento.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { ApiService } from 'src/app/services/api.service';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-descripcion-del-estacionamiento',
@@ -9,16 +11,59 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class DescripcionDelEstacionamientoPage implements OnInit {
   data: any;
   text: string = 'new';
+  advertisements: any[] = [];
+  isLoading: boolean = false;
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private api: ApiService,
+    private storage: StorageService
+  ) {
+    console.log('Constructor ejecutado');
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.loadContent(); // Llamar al método sin await ya que ngOnInit no es async
+  }
+
+  ionViewWillEnter() {
+    console.log('ionViewWillEnter ejecutado - Descripción del estacionamiento');
+    this.loadContent();
+    // También puedes cargar contenido aquí si prefieres seguir el ciclo de vida de Ionic
+  }
 
   handleNavigateTo(route: string) {
     if (route) {
       this.router.navigate([route]);
     }
   }
+
+  async loadContent() {
+    this.isLoading = true; // Asegurar que loading está activado
+    const token = await this.storage.getItem('token');
+
+    this.api.getAds(token).subscribe(
+      (response: any) => {
+        this.advertisements = response.data;
+        console.log(
+          '🚀 ~ DescripcionDelEstacionamientoPage ~ loadContent ~ this.advertisements:',
+          this.advertisements
+        );
+
+        if(this.advertisements.length > 0) {
+          this.router.navigate(['tab-home'])
+
+        }
+
+        this.isLoading = false; // Finalizar loading después de cargar los anuncios
+      },
+      (error: any) => {
+        console.error('Error al cargar los anuncios:', error);
+        this.isLoading = false; // Finalizar loading incluso en caso de error
+      }
+    );
+  }
+
   navigate() {
     this.router.navigate(['/descripcion-del-espacio']);
   }
